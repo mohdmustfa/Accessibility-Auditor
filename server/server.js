13d20327cbe3ca15a8f13f4225b9b73c39a7584a
mohdmustfa <md.mustfamalik786@gmail.com>
2026-05-13 16:40:11 +0530

    removed dead code

--- server/server.js
+++ server/server.js
@@ -7,14 +7,6 @@ import {
 
 const app = express();
 
-// Handle PNA preflight
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Private-Network", "true");
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST"],
@@ -47,15 +39,12 @@ app.post("/scan", async (req, res) => {
 
     return res.status(500).json({
         success: false,
-        // message: "Sorry, could not fetch data for this URL. Check your URL and try again.", 
-        message: error.message,
+        message: "Sorry, could not fetch data for this URL. Check your URL and try again.", 
+        // message: error.message,
     });
   }
 });
 
-// app.listen(5000, () => {
-//   console.log("Server running on port 5000");
-// });
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
